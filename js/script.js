2b3ec9022bd8957cbbe3fed410ed0521b2082059
alex2000d <demarcoalessandro777@gmail.com>
2024-07-18 15:46:17 +0200

    [UPD] created the arrey object

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+// creo la struttura per vuejs
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return {
+             todolist: [
+                 {
+                     done: true ,
+                     text: 'andare a fare la spesa',
+                 },
+                  {
+                     done: false ,
+                     text: 'fare l\'esercizio di oggi ',
+                 },
+                  {
+                     done: true ,
+                     text: "capire come diventare ricchi da fare schifo",
+                 },
+                  {
+                     done: true ,
+                     text: 'investire in cripto',
+                 },
+                  {
+                     done: false ,
+                     text: 'guardare un film',
+                 }
+             ]
+        }
+    },
+  
+    methods: {
+     
+  }
+}).mount('#app');
